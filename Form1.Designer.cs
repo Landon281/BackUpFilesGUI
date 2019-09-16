de2aad50e1cfd087d8dc09aa2a58d4d77ba9097c
Landon <49599350+Landon281@users.noreply.github.com>
2019-09-16 00:42:19 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace BackUpSave {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnFileRelease = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnEraseFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 185);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(606, 303);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnFileRelease
+            // 
+            this.btnFileRelease.AllowDrop = true;
+            this.btnFileRelease.Location = new System.Drawing.Point(110, 34);
+            this.btnFileRelease.Name = "btnFileRelease";
+            this.btnFileRelease.Size = new System.Drawing.Size(310, 101);
+            this.btnFileRelease.TabIndex = 2;
+            this.btnFileRelease.Text = "Drag New Files Here";
+            this.btnFileRelease.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(624, 185);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(395, 303);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Files That Will Be Backed Up:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(797, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Console:";
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.AllowDrop = true;
+            this.btnBackup.Location = new System.Drawing.Point(610, 34);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(310, 101);
+            this.btnBackup.TabIndex = 6;
+            this.btnBackup.Text = "Back Up Files";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            // 
+            // btnEraseFile
+            // 
+            this.btnEraseFile.Location = new System.Drawing.Point(998, 496);
+            this.btnEraseFile.Name = "btnEraseFile";
+            this.btnEraseFile.Size = new System.Drawing.Size(21, 23);
+            this.btnEraseFile.TabIndex = 7;
+            this.btnEraseFile.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(855, 501);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Reset files to be backed up";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 531);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnEraseFile);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.btnFileRelease);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Back Up Your Files!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnFileRelease;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnEraseFile;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿//Started 9/15/2019 by Landon Hise
+//Use: To back up files utilizing a GUI and persistently store location of previously backed up files
+//GUI displays to user the source files for backup, and any error messages
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BackUpSave {
+
+    public partial class Form1 : Form {
+
+       
+
+        public Form1() {
+            InitializeComponent();
+            this.btnFileRelease.DragDrop += new DragEventHandler(this.btnFileRelease_DragDrop);
+            this.btnFileRelease.DragEnter += new DragEventHandler(this.btnFileRelease_DragEnter);         
+        }
+
+        
+
+
+        //Generate file name with semi-unique key
+        public string GenerateFileKey() {
+            return ("_Backup[" + DateTime.Now.Month + DateTime.Now.Day + DateTime.Now.Year + "-" + DateTime.Now.Hour + DateTime.Now.Millisecond + "]");
+        }
+        //capture current users name (used to create Backup directory in appropriate place)
+        static String currentUser = Environment.UserName;
+        
+       
+
+       
+
+        //method that convers a string array to a normal string via StringBuilder
+        static string ConvertStringArray(string[] array) {
+       
+            StringBuilder storage = new StringBuilder();
+
+            foreach (string value in array) {
+                storage.Append(value);
+            }
+
+            return (storage.ToString());
+        }//end convert method
+
+
+
+        //On Form Load
+        private void Form1_Load(object sender, EventArgs e) {
+            btnFileRelease.AllowDrop = true;
+
+            string pathToBackups = (@"C:\Users\" + currentUser + @"\Backups");
+            string logFile = Path.Combine(pathToBackups, "filesToBackup.txt");
+
+            //On form load, create a directory titled "Backups" in current user's home directory
+            //will be ignored automatically if directory already exists
+            Directory.CreateDirectory(pathToBackups);
+
+            if (!File.Exists(logFile)) {
+                File.Create(logFile).Close();
+            }
+
+            long logFileLength = new FileInfo(logFile).Length;
+
+            if (logFileLength == 0) {
+                //do nothing 
+            }
+            else {
+
+                StreamReader readFilePaths = new StreamReader(logFile);
+                string temp = "";
+
+                while ((temp = readFilePaths.ReadLine()) != null) {
+                    listBox1.Items.Add(temp);
+                }
+  
+                readFilePaths.Close();
+            }
+            
+
+        }//end Form1_Load
+
+
+
+        private void btnFileRelease_DragDrop(object sender, DragEventArgs e) {
+
+            string pathToBackups = (@"C:\Users\" + currentUser + @"\Backups");
+            string logFile = Path.Combine(pathToBackups, "filesToBackup.txt");
+
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string sourceFile = ConvertStringArray(s);
+
+
+            //Build file name of newly created file by extracting info from source file
+            string copyFileName = Path.GetFileNameWithoutExtension(sourceFile);
+            string copyExtension = Path.GetExtension(sourceFile);
+            string keyedFileName = (copyFileName + GenerateFileKey() + copyExtension);
+
+            //combine path to backups directory with semi-unique file name
+            string destFile = Path.Combine(pathToBackups, keyedFileName);
+            string successfulCopyMsg = "File successfully copied!";
+            string duplicateCopyMsg = "File is already scheduled to backed up!";
+
+            
+
+
+            if (File.ReadAllText(logFile).Contains(sourceFile)) {  
+                listBox2.Items.Add(duplicateCopyMsg);
+            }
+            else {
+
+                File.AppendAllText(logFile, sourceFile + Environment.NewLine);
+
+                listBox1.Items.Add(sourceFile);
+                listBox2.Items.Add(successfulCopyMsg);
+
+                //copy file to destination
+                File.Copy(sourceFile, destFile);
+               
+            }
+
+        }//end textBoxFile_DragDrop
+
+
+        private void btnFileRelease_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }//end textBoxFile_DragEnter
+
+
+    }
+}
